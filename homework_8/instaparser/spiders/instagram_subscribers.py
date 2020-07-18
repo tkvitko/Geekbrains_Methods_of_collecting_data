febafbc20d5e1c990d8acb2004db19607b61d8bc
Taras Kvitko <tkvitko@gmail.com>
2020-07-19 00:31:29 +0300

    Homework 8 remove commented code

--- homework_8/instaparser/spiders/instagram_subscribers.py
+++ homework_8/instaparser/spiders/instagram_subscribers.py
@@ -82,43 +82,6 @@ class InstagramSubscribersSpider(scrapy.Spider):
             )
         yield item  # В пайплайн
 
-    # def user_data_parse(self, response: HtmlResponse, username):
-    #     user_id = self.fetch_user_id(response.text, username)  # Получаем id пользователя
-    #     variables = {'id': user_id,  # Формируем словарь для передачи даных в запрос
-    #                  'first': 12}  # 12 постов. Можно больше (макс. 50)
-    #     url_posts = f'{self.graphql_url}query_hash={self.posts_hash}&{urlencode(variables)}'  # Формируем ссылку для получения данных о постах
-    #     yield response.follow(
-    #         url_posts,
-    #         callback=self.user_posts_parse,
-    #         cb_kwargs={'username': username,
-    #                    'user_id': user_id,
-    #                    'variables': deepcopy(variables)}  # variables ч/з deepcopy во избежание гонок
-    #     )
-    #
-    # def user_posts_parse(self, response: HtmlResponse, username, user_id,
-    #                      variables):  # Принимаем ответ. Не забываем про параметры от cb_kwargs
-    #     j_data = json.loads(response.text)
-    #     page_info = j_data.get('data').get('user').get('edge_owner_to_timeline_media').get('page_info')
-    #     if page_info.get('has_next_page'):  # Если есть следующая страница
-    #         variables['after'] = page_info['end_cursor']  # Новый параметр для перехода на след. страницу
-    #         url_posts = f'{self.graphql_url}query_hash={self.posts_hash}&{urlencode(variables)}'
-    #         yield response.follow(
-    #             url_posts,
-    #             callback=self.user_posts_parse,
-    #             cb_kwargs={'username': username,
-    #                        'user_id': user_id,
-    #                        'variables': deepcopy(variables)}
-    #         )
-    #     posts = j_data.get('data').get('user').get('edge_owner_to_timeline_media').get('edges')  # Сами посты
-    #     for post in posts:  # Перебираем посты, собираем данные
-    #         item = InstaparserItem(
-    #             user_id=user_id,
-    #             photo=post['node']['display_url'],
-    #             likes=post['node']['edge_media_preview_like']['count'],
-    #             post=post['node']
-    #         )
-    #     yield item  # В пайплайн
-
     # Получаем токен для авторизации
     def fetch_csrf_token(self, text):
         matched = re.search('\"csrf_token\":\"\\w+\"', text).group()
